008b83dec0322affaa00559e73d8e02844cdc223
Alexey Artyushin <shinxey1@gmail.com>
2023-09-24 22:25:45 +0600

    refactor mandelbrot drawing function

--- fractal/mandelbrot/mandelbrot.go
+++ fractal/mandelbrot/mandelbrot.go
@@ -14,24 +14,17 @@ func Draw(width int, height int, iters int) []float64 {
     for x := 0; x < width; x++ {
         go func(x int) {
             for y := 0; y < height; y++ {
-                x0 := -1.167 + 0.0005 / fWidth * float64(x)
-                y0 := 0.235 + 0.0005 / fHeight * float64(y)
+                z0 := complex(
+                    -1.167 + 0.0005 / fWidth * float64(x), 
+                    0.235 + 0.0005 / fHeight * float64(y),
+                )
 
-                var xn float64 = 0
-                var yn float64 = 0
+                zn := z0
 
                 doneIters := 0
 
-                for k := 0; k < iters; k++ {
-                    if xn * xn + yn * yn > 4 {
-                        break
-                    }
-
-                    xn1 := xn * xn - yn * yn + x0
-                    yn1 := 2 * xn * yn + y0
-
-                    xn = xn1
-                    yn = yn1
+                for (real(zn) * real(zn) + imag(zn) * imag(zn) <= 4) && (doneIters < iters) {
+                    zn = zn * zn + z0
 
                     doneIters += 1
                 }
